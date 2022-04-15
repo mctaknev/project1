b25012de54f4214268835b30f294b18ee0b040a3
mctaknev <100849698+mctaknev@users.noreply.github.com>
2022-04-15 23:56:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+let bgColor = document.getElementById("container");
+let colorCode = document.getElementById("hexCode");
+
+function changeBg1(){
+    alert("hi");
+    bgColor.style.backgroundColor= "#e0e0e0";
+    colorCode.textContent="#e0e0e0";
+}
+
+function changeBg2(){
+    bgColor.style.backgroundColor= "#6fcf97";
+    colorCode.textContent="#6fcf97";
+}
+
+function changeBg3(){
+    bgColor.style.backgroundColor= "#56ccf2";
+    colorCode.textContent="#56ccf2";
+}
+
+function changeBg4(){
+    bgColor.style.backgroundColor= "#bb6bd9";
+    colorCode.textContent="#bb6bd9";
+}
